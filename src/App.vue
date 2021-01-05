5823c03fde9eeeb3ba7c8dbd35d6844b05adc0ba
Emil Carlén <emil.j.carlen@gmail.com>
2021-01-05 17:35:28 +0100

    Fix server ip and components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
   <div id="app">
-    <div class="content">
-      <h1>incel gejming klubb</h1>
-      <p v-if="!info" class="info">incel.gay &mdash; Laddar...</p>
-      <div v-else>
-        <p class="info" v-if="info.online">
-          <span class="dot online"></span> incel.gay &mdash;
-          {{ info.players.online }}/{{ info.players.max }} spelare online ({{
-            info.version
-          }})
-        </p>
-        <p class="info" v-else>
-          <span class="dot offline"></span> incel.gay &mdash; Offline
-        </p>
-      </div>
-    </div>
+    <Hero />
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import Hero from "./components/Hero";
 
 export default {
   name: "App",
-  data() {
-    return {
-      info: null,
-    };
-  },
-  mounted() {
-    axios
-      .get("https://api.mcsrvstat.us/2/incel.gay")
-      .then((response) => (this.info = response.data));
+  components: {
+    Hero,
   },
 };
 </script>
@@ -54,40 +33,5 @@ html {
   flex-direction: column;
   justify-content: center;
   padding: 0 128px;
-}
-
-.content {
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-h1 {
-  font-size: 4em;
-  margin: 0 0 10px 0;
-  font-weight: 700;
-}
-
-.info {
-  margin: 0;
-  font-size: 1.5em;
-  font-weight: 300;
-}
-
-.dot {
-  height: 15px;
-  width: 15px;
-  border-radius: 50%;
-  display: inline-block;
-  margin-right: 5px;
-}
-
-.dot.online {
-  background-color: #5cff7a;
-}
-
-.dot.offline {
-  background-color: #ff5757;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="hero">
+    <h1>incel gejming klubb</h1>
+    <p v-if="errored" class="info error">Kunde inte ladda serverstatus</p>
+    <div v-else>
+      <p v-if="loading" class="info">
+        <span class="dot loading"></span> mc.incel.gay &mdash; Laddar...
+      </p>
+      <div v-else>
+        <p class="info" v-if="info.online">
+          <span class="dot online"></span> mc.incel.gay &mdash;
+          {{ info.players.online }}/{{ info.players.max }} spelare online
+          <span style="color: #808080">(MC {{ info.version }})</span>
+        </p>
+        <p class="info" v-else>
+          <span class="dot offline"></span> mc.incel.gay &mdash; Offline
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Hero",
+  data() {
+    return {
+      loading: true,
+      info: null,
+      errored: false,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.mcsrvstat.us/2/mc.incel.gay")
+      .then((response) => (this.info = response.data))
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
+
+<style>
+.hero {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+
+h1 {
+  font-size: 4em;
+  margin: 0 0 10px 0;
+  font-weight: 700;
+}
+
+.info {
+  margin: 0;
+  font-size: 1.5em;
+  font-weight: 300;
+}
+
+.dot {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.info.error {
+  color: #ff5757;
+}
+
+.dot.loading {
+  background-color: #477bff;
+}
+
+.dot.online {
+  background-color: #5cff7a;
+}
+
+.dot.offline {
+  background-color: #ff5757;
+}
+</style>
