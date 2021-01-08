821aba9ad32de653f0017cf284fbcece5b25cc5b
Emil Carlén <emil.j.carlen@gmail.com>
2021-01-08 15:58:02 +0100

    Even moe guy

--- src/components/BottomBanner.vue
+++ src/components/BottomBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-2 flex absolute blurple gradient bottom-0 right-0 w-full py-4 md:px-8 xl:px-72 2xl:px-96 text-xl justify-between items-center"
+    class="px-2 flex absolute blurple gradient bottom-0 right-0 w-full py-4 md:px-8 xl:px-72 2xl:px-96 xl:justify-around text-xl justify-between items-center"
   >
     <p class="inline">incel gejming klubb: kuk-o-boll-unionen på Discord</p>
     <a
@@ -11,7 +11,8 @@
     </a>
     <img
       src="@/assets/images/discord.png"
-      class="hidden 2xl:block absolute left-16 bottom-1/3 rounded-lg w-32 hover:rounded-lg"
+      class="hidden 2xl:block absolute left-16 bottom-1/3 rounded-2xl w-32 
+      transform duration-300 hover:-translate-y-2 hover:scale-105 hover:shadow-lg"
     />
   </div>
 </template>
